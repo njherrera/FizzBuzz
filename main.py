169d6bb09cba3ba53452769e51f66cbaf23fa62e
njherrera <nathan.j.herrera@gmail.com>
2021-08-19 18:48:49 -0700

    added some comments

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 def fizzBuzz(number):
-    for x in range(1, number):
-        if x % 3 == 0 and x % 5 == 0:
+    for x in range(1, number): # starting from 1, count up to the input number
+        if x % 3 == 0 and x % 5 == 0: # if current number is divisible by 3 and 5, print FizzBuzz
             print("FizzBuzz")
-        elif x % 3 == 0:
+        elif x % 3 == 0: # if current number is divisible only by 3, print Fizz
             print("Fizz")
-        elif x % 5 == 0:
+        elif x % 5 == 0: # if current number is divisible only by 5, print Buzz
             print("Buzz")
         else: print(x)
 
