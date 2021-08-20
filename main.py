3243084c4daa1d496fdead0d480aad752f2238c5
njherrera <nathan.j.herrera@gmail.com>
2021-08-19 18:47:08 -0700

    first commit, just a simple FizzBuzz program

---
+++ main.py
@@ -0,0 +1,27 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+def fizzBuzz(number):
+    for x in range(1, number):
+        if x % 3 == 0 and x % 5 == 0:
+            print("FizzBuzz")
+        elif x % 3 == 0:
+            print("Fizz")
+        elif x % 5 == 0:
+            print("Buzz")
+        else: print(x)
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    fizzBuzz(20)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
